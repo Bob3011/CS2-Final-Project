0d83c51c23cede5793ee12f7f1ac1fbbf48fa12a
Bob3011 <abdallah.ibrahim3011@aucegypt.edu>
2023-04-17 02:42:02 +0200

    second commit after fully understanding and completign the program run

--- program_run.h
+++ program_run.h
@@ -4,15 +4,14 @@
 #include <iostream>
 #include <map>
 #include <fstream>
+#include <chrono>
 #include "Document.h"
 #include "File_compilation.h"
-
+#include "structPlagerizedSentence.h"
 
 using namespace std;
-struct plagiarized_sentence {
-	string content;
-	string doc_dir;
-};
+using namespace std::chrono;
+
 
 class Program_run : public Document, public file_compialtion
 {
@@ -23,20 +22,135 @@ private:
 	vector<plagiarized_sentence> Rabin_result;
 	vector<plagiarized_sentence> Hamming_result;
 	vector<plagiarized_sentence> Booyer_result;
+	vector<plagiarized_sentence> BrutForce_result;
+	long long time_KMP;
+	long long time_Rabin;
+	long long time_Booyer;
+	long long time_Hamming;
+	long long time_BrutForce;
 
 public:
 	map<string, int> results_KMP;
 	map<string, int> results_Rabin;
 	map<string, int> results_Booyer;
 	map<string, int> results_Hamming;
-	/*long long time_KMP;
-	long long time_Rabin;
-	long long time_Booyer;
-	long long time_Hamming;*/
-	void check_match_kmp();
-	void check_match_rabin();
-	void check_match_Booyer();
-	void check_match_hamming();
+
+	void check_match_kmp()
+	{
+		auto start = high_resolution_clock::now();
+
+		for (int i = 0; i < doc.get_sentenses().size(); i++) 
+		{
+			for (int j = 0; j < c.get_docs().size(); j++) 
+			{
+				if (c.kmpSearch(doc.manip_sentenses(i), c.manip_docs(j).get_txt())) 
+				{
+					plagiarized_sentence S;
+					S.content = doc.manip_sentenses(i);
+					S.doc_dir = c.manip_docs(j).get_directory();
+					//results_KMP[S.doc_dir]++;
+					KMP_result.push_back(S);
+					break;
+				}
+			}
+		}
+
+		auto stop = high_resolution_clock::now();
+		auto time_Rabin = duration_cast<microseconds>(stop - start);
+	}
+	void check_match_rabin()
+	{
+		auto start = high_resolution_clock::now();
+
+		for (int i = 0; i < doc.get_sentenses().size(); i++)
+		{
+			for (int j = 0; j < c.get_docs().size(); j++)
+			{
+				if (c.rabinKarp(doc.manip_sentenses(i), c.manip_docs(j).get_txt()))
+				{
+					plagiarized_sentence S;
+					S.content = doc.manip_sentenses(i);
+					S.doc_dir = c.manip_docs(j).get_directory();
+					//results_KMP[S.doc_dir]++;
+					Rabin_result.push_back(S);
+					break;
+				}
+			}
+		}
+
+		auto stop = high_resolution_clock::now();
+		auto time_Booyer = duration_cast<microseconds>(stop - start);
+	}
+	void check_match_Booyer()
+	{
+		auto start = high_resolution_clock::now();
+
+		for (int i = 0; i < doc.get_sentenses().size(); i++)
+		{
+			for (int j = 0; j < c.get_docs().size(); j++)
+			{
+				if (c.boyerMoore(doc.manip_sentenses(i), c.manip_docs(j).get_txt()))
+				{
+					plagiarized_sentence S;
+					S.content = doc.manip_sentenses(i);
+					S.doc_dir = c.manip_docs(j).get_directory();
+					//results_KMP[S.doc_dir]++;
+					Booyer_result.push_back(S);
+					break;
+				}
+			}
+		}
+
+		auto stop = high_resolution_clock::now();
+		auto time_KMP = duration_cast<microseconds>(stop - start);
+	}
+	void check_match_hamming()
+	{
+		auto start = high_resolution_clock::now();
+
+		for (int i = 0; i < doc.get_sentenses().size(); i++)
+		{
+			for (int j = 0; j < c.get_docs().size(); j++)
+			{
+				if (c.hammingDistance(doc.manip_sentenses(i), c.manip_docs(j).get_txt()))
+				{
+					plagiarized_sentence S;
+					S.content = doc.manip_sentenses(i);
+					S.doc_dir = c.manip_docs(j).get_directory();
+					//results_KMP[S.doc_dir]++;
+					Hamming_result.push_back(S);
+					break;
+				}
+			}
+		}
+
+		auto stop = high_resolution_clock::now();
+		auto time_Hamming = duration_cast<microseconds>(stop - start);
+	}
+
+	void check_match_brutForce()
+	{
+		auto start = high_resolution_clock::now();
+
+		for (int i = 0; i < doc.get_sentenses().size(); i++)
+		{
+			for (int j = 0; j < c.get_docs().size(); j++)
+			{
+				if (c.bruteForceStringMatch(doc.manip_sentenses(i), c.manip_docs(j).get_txt()))
+				{
+					plagiarized_sentence S;
+					S.content = doc.manip_sentenses(i);
+					S.doc_dir = c.manip_docs(j).get_directory();
+					//results_KMP[S.doc_dir]++;
+					BrutForce_result.push_back(S);
+					break;
+				}
+			}
+		}
+
+		auto stop = high_resolution_clock::now();
+		auto time_BrutForce = duration_cast<microseconds>(stop - start);
+	}
 
 
 	void get_doc(string file_name)
---
+++ structPlagerizedSentence.h
@@ -0,0 +1,11 @@
+#ifndef structPS
+#define structPS
+
+#include <iostream>
+using namespace std;
+
+struct plagiarized_sentence {
+	string content;
+	string doc_dir;
+};
+#endif // !1
